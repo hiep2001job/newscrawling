e9aeb41de2fa338b2f68e5135f8f1cf5e3a5c250
Hiep <hiep2001.job@gmail.com>
2022-08-28 20:21:09 +0700

    production fix

--- index.php
+++ index.php
@@ -11,12 +11,29 @@ header('Access-Control-Allow-Methods: GET, POST');
 
 header("Access-Control-Allow-Headers: X-Requested-With");
 
+//For dev localhost
+// $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+// $uri = explode( '/', $uri );
+
  
+// if ((isset($uri[2]) && $uri[2] != 'news') || !isset($uri[3])) {
+//     header("HTTP/1.1 404 Not Found");
+//     exit();
+// }
+ 
+// require PROJECT_ROOT_PATH . "/Controllers/Api/NewsController.php";
+ 
+// $objFeedController = new NewsController();
+// $strMethodName = $uri[3] . 'Action';
+// $objFeedController->{$strMethodName}();
+
+
+//For production
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $uri = explode( '/', $uri );
 
  
-if ((isset($uri[2]) && $uri[2] != 'news') || !isset($uri[3])) {
+if ((isset($uri[1]) && $uri[1] != 'news') || !isset($uri[2])) {
     header("HTTP/1.1 404 Not Found");
     exit();
 }
@@ -24,33 +41,5 @@ if ((isset($uri[2]) && $uri[2] != 'news') || !isset($uri[3])) {
 require PROJECT_ROOT_PATH . "/Controllers/Api/NewsController.php";
  
 $objFeedController = new NewsController();
-$strMethodName = $uri[3] . 'Action';
+$strMethodName = $uri[2] . 'Action';
 $objFeedController->{$strMethodName}();
-
-// $controller='';
-// $method='';
-// $params='';
-
-// if(file_exists("../app/controllers/" . strtolower($url[2]) . ".php"))
-// 		{
-
-// 			$controller = strtolower($url[2]);
-// 			unset($url[2]);
-// 		}
-
-// 		require "../app/controllers/" . $this->controller . ".php";
-// 		$this->controller = new $this->controller;
-
-// 		if(isset($url[1]))
-// 		{
-// 			$url[1] = strtolower($url[1]);
-// 			if(method_exists($this->controller, $url[1]))
-// 			{
-// 				$this->method = $url[1];
-// 				unset($url[1]);
-// 			}
-// 		}
-
-// 		$this->params = (count($url) > 0) ? $url : ["home"];
-		
-// 		call_user_func_array([$this->controller,$this->method], $this->params);
